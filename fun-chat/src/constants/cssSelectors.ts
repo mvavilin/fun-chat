7db1676733733d8b5d508c4fa72f378fd735517c
mvavilin <realvavilin@yandex.ru>
2026-02-05 23:01:45 +0300

    refactor: move selectors and text to constants

---
+++ fun-chat/src/constants/cssSelectors.ts
@@ -0,0 +1,33 @@
+const CSS_CLASSES = {
+  APP: ['app'],
+
+  BTN: ['btn'],
+  BTN_SECONDARY: ['btn', 'secondary'],
+  BTN_DANGER: ['btn', 'danger'],
+  BTN_SMALL: ['btn', 'small'],
+  BTN_LARGE: ['btn', 'large'],
+
+  INPUT: ['input'],
+  INPUT_SMALL: ['input', 'small'],
+  INPUT_LARGE: ['input', 'large'],
+  INPUT_ROUNDED: ['input', 'rounded'],
+  TEXTAREA: ['textarea'],
+
+  CONTAINER: ['container'],
+
+  H1: ['h1'],
+  H2: ['h2'],
+  PAGE_TITLE: ['page-title'],
+  SECTION_TITLE: ['section-title'],
+
+  FORM: ['form'],
+
+  SECTION: ['section'],
+
+  LOADING: ['loading'],
+  WITH_ICON: ['with-icon'],
+
+  SPINNER: ['spinner'],
+};
+
+export default CSS_CLASSES;
