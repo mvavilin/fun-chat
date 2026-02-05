a633a1eb940e0bbe95993dbc8df319cbd9d0e30d
mvavilin <realvavilin@yandex.ru>
2026-02-05 15:59:34 +0300

    chore: install SCSS and configure aliases

--- fun-chat/vite.config.js
+++ fun-chat/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/mvavilin-JSFE2025Q3/',
 
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@api': resolve(__dirname, 'src/api'),
-      '@components': resolve(__dirname, 'src/components'),
-      '@pages': resolve(__dirname, 'src/pages'),
-      '@state': resolve(__dirname, 'src/state'),
-      '@types': resolve(__dirname, 'src/types'),
-      '@utils': resolve(__dirname, 'src/utils'),
-    },
-  },
+  plugins: [tsconfigPaths()],
 });
