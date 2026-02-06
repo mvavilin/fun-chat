54196caae34b8f1dd00f2dce4a7a66fe497b8e68
mvavilin <realvavilin@yandex.ru>
2026-02-06 20:50:13 +0300

    style: add gradient background

--- fun-chat/src/pages/LoginPage/LoginPage.ts
+++ fun-chat/src/pages/LoginPage/LoginPage.ts
@@ -1,18 +1,20 @@
-import { PageBuilder } from '@utils';
+import { ElementBuilder, PageBuilder } from '@utils';
 import type { PageComponentOptions } from '@types';
 import { LoginForm } from '@components';
 
 export default class LoginPage extends PageBuilder {
-  constructor({
-    id = 'login-page',
-    classes = ['login-page', 'container'],
-  }: PageComponentOptions = {}) {
+  private container: ElementBuilder;
+
+  constructor({ id = 'login-page', classes = ['login-page'] }: PageComponentOptions = {}) {
     super({ id, classes });
+
+    this.container = new ElementBuilder({ classes: ['container', 'login-page-container'] });
 
     this.render();
   }
 
   private render(): void {
-    this.addChild(new LoginForm());
+    this.container.addChild(new LoginForm());
+    this.addChild(this.container);
   }
 }
